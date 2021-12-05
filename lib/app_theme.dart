41cc68fe719b3b6fdccb7d1a47b6324cb4ff49ea
Andrew <johndoe@e.com>
2021-12-05 09:43:36 +0545

    final

---
+++ lib/app_theme.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:chuckapi/route_animation.dart';
+
+ThemeData darkTheme() {
+  return ThemeData(
+    brightness: Brightness.dark,
+    pageTransitionsTheme: PageTransitionsTheme(
+      builders: {
+        TargetPlatform.android: CustomPageTransitionBuilder(),
+      },
+    ),
+    primarySwatch: Colors.teal,
+    errorColor: Colors.pink,
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      foregroundColor: Colors.black,
+      backgroundColor: Colors.amber,
+    ),
+    primaryColor: Colors.amber,
+    canvasColor: const Color.fromRGBO(12, 12, 12, 0.96),
+    textTheme: ThemeData.dark().textTheme.copyWith(
+          bodyText1: const TextStyle(
+            color: Colors.white,
+            fontSize: 17,
+            fontFamily: 'SourceSansPro',
+          ),
+          bodyText2: const TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontFamily: 'Chilanka',
+          ),
+          headline1: const TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Chilanka',
+          ),
+          headline2: const TextStyle(
+            color: Colors.pinkAccent,
+            fontSize: 18,
+            fontFamily: 'SourceSansPro',
+          ),
+          headline3: const TextStyle(
+            color: Colors.white,
+            fontSize: 17,
+          ),
+          headline4: const TextStyle(
+            color: Colors.white,
+            fontFamily: 'SourceSansPro',
+            fontSize: 20.0,
+          ),
+          headline5: const TextStyle(
+            color: Colors.white54,
+            fontFamily: 'SourceSansPro',
+            fontSize: 13.0,
+          ),
+          headline6: const TextStyle(
+            fontSize: 20,
+            fontFamily: 'SourceSansPro',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+  );
+}
+
+ThemeData lightTheme() {
+  return ThemeData(
+    pageTransitionsTheme: PageTransitionsTheme(
+      builders: {
+        TargetPlatform.android: CustomPageTransitionBuilder(),
+      },
+    ),
+    primarySwatch: Colors.teal,
+    errorColor: Colors.pink,
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      foregroundColor: Colors.black,
+      backgroundColor: Colors.amber,
+    ),
+    primaryColor: Colors.amber,
+    canvasColor: const Color.fromRGBO(255, 254, 229, 0.96),
+    textTheme: ThemeData.light().textTheme.copyWith(
+          bodyText1: const TextStyle(
+            fontSize: 17,
+            fontFamily: 'SourceSansPro',
+          ),
+          bodyText2: const TextStyle(
+            fontSize: 22,
+            fontFamily: 'Chilanka',
+          ),
+          headline1: const TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Chilanka',
+          ),
+          headline2: const TextStyle(
+            color: Colors.pinkAccent,
+            fontSize: 18,
+            fontFamily: 'SourceSansPro',
+          ),
+          headline3: const TextStyle(
+            color: Colors.black,
+            fontSize: 17,
+          ),
+          headline4: const TextStyle(
+            color: Colors.black,
+            fontFamily: 'SourceSansPro',
+            fontSize: 20.0,
+          ),
+          headline5: const TextStyle(
+            color: Colors.black54,
+            fontFamily: 'SourceSansPro',
+            fontSize: 13.0,
+          ),
+          headline6: const TextStyle(
+            fontSize: 20,
+            fontFamily: 'SourceSansPro',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:chuckapi/app_theme.dart';
+import 'package:chuckapi/config.dart';
+import 'package:chuckapi/theme.dart';
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 import 'package:chuckapi/joke.dart';
 
 void main() {
   setPathUrlStrategy();
-  runApp(const MyApp());
+  runApp(const MyHomePage());
 }
 
 Future<Joke> fetchJoke() async {
@@ -29,27 +33,8 @@ Future<Joke> fetchJoke() async {
   }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ChuckAPI',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      darkTheme: ThemeData.dark(),
-      themeMode: ThemeMode.system,
-      home: const MyHomePage(title: 'ChuckAPI'),
-    );
-  }
-}
-
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -62,40 +47,64 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     futureAlbum = fetchJoke();
+    _loadStart();
+    currentTheme.addListener(() {
+      setState(() {});
+    });
+  }
+
+  _loadStart() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    MyTheme.isDark = (prefs.getBool('isDark') ?? false);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Center(
-          child: FutureBuilder<Joke>(
-            future: futureAlbum,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Padding(
-                  padding: const EdgeInsets.all(14.0),
-                  child: Text(snapshot.data!.value),
-                );
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-              return const CircularProgressIndicator();
-            },
+    return MaterialApp(
+      title: 'ChuckAPI',
+      darkTheme: darkTheme(), // dark theme
+      theme: lightTheme(), // light theme
+      themeMode: currentTheme.currentTheme(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('ChuckApi'),
+          actions: [
+            IconButton(
+              onPressed: () async {
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                currentTheme.switchTheme(prefs);
+              },
+              icon: const Icon(Icons.dark_mode),
+            ),
+          ],
+        ),
+        body: Center(
+          child: Center(
+            child: FutureBuilder<Joke>(
+              future: futureAlbum,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Padding(
+                    padding: const EdgeInsets.all(14.0),
+                    child: Text(snapshot.data!.value),
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+                return const CircularProgressIndicator();
+              },
+            ),
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          setState(() {
-            futureAlbum = fetchJoke();
-          });
-        },
-        tooltip: 'Next Joke',
-        child: const Icon(Icons.refresh_outlined),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            setState(() {
+              futureAlbum = fetchJoke();
+            });
+          },
+          tooltip: 'Next Joke',
+          child: const Icon(Icons.refresh_outlined),
+        ),
       ),
     );
   }
